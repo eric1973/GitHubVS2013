bdac84873cc5282f57106c5908781b98111375e1
erich brunner <erich.brunner@live.at>
2014-10-19 18:06:09 +0200

    test async await event handler

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public delegate void EventHandler();
+
+class Program
+{
+    public static event EventHandler _show;
+
+    static void Main()
+    {
+        // Add event handlers to Show event.
+        //_show += new EventHandler(Dog);
+        //_show += new EventHandler(Cat);
+        //_show += new EventHandler(Mouse);
+        //_show += new EventHandler(Mouse);
+
+        _show += async () =>
+        {
+            await Task.Delay(2000);
+            Console.WriteLine("async call");
+        };
+
+        // Invoke the event.
+        _show.Invoke();
+        Console.WriteLine("after invoke");
+        Console.ReadLine();
+    }
+
+    static async void Cat()
+    {
+        Console.WriteLine("Cat");
+    }
+
+    static async void Dog()
+    {
+        Console.WriteLine("Dog");
+    }
+
+    static async void Mouse()
+    {
+        Console.WriteLine("Mouse");
+    }
+}
